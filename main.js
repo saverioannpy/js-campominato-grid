45eb2589fa9d7b6be4fde336194fb49d91f35eb5
saverioannpy <saverio.annunziata@outlook.com>
2023-11-16 17:11:32 +0100

    feat: main.js push

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
 
 /** Funzioni **/
 
-function createTable(){
+function createTable(){ //funzione per la creazione della griglia di gioco
     const gameGrid = document.querySelector('div.game-grid'); //seleziono la griglia di gioco
     console.log(gameGrid);
     gameGrid.innerHTML = "";
     const difficultySelected = document.getElementById("difficulty-select").value; //controllo la difficoltà selezionata dall'utente
     console.log("Giochiamo a difficoltà: " + difficultySelected);
     switch (difficultySelected) {
-        case "easy":
+        case "easy": //creazione griglia 10x10
             for(let i = 1 ; i <= 100; i++){
                 let myCell = document.createElement('div'); //creo un elemento div per la cella
                 myCell.classList.add("cell" , "easy"); //do alla cella le sue relative classi
@@ -20,7 +20,7 @@ function createTable(){
             }
             break;
 
-        case "medium":
+        case "medium": //creazione griglia 9x9
             for(let i = 1 ; i <= 81; i++){
                 let myCell = document.createElement('div'); //creo un elemento div per la cella
                 myCell.classList.add("cell" , "medium"); //do alla cella le sue relative classi
@@ -30,7 +30,7 @@ function createTable(){
             }
             break;
 
-        case "hard":
+        case "hard": //creazione griglia 7x7
             for(let i = 1 ; i <= 49; i++){
                 let myCell = document.createElement('div'); //creo un elemento div per la cella
                 myCell.classList.add("cell" , "hard"); //do alla cella le sue relative classi
